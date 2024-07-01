64169f04ae4f76687097c297f837a2621b90b643
jeanlucndato <jeanlucndato@gmail.com>
2024-07-01 16:34:22 -0400

    on continue mais le proble est avec jquery

--- reserver/reserver.py
+++ reserver/reserver.py
@@ -1,3 +1,5 @@
+from serena_app.models import Product
+
 class Reserver:
     def __init__(self, request):
         self.session = request.session
@@ -11,3 +13,22 @@ class Reserver:
             
     #Make sure is available on all pages of site
         self.reserver = reserver
+        
+    def add(self, product):
+        product_id = str(product.id)
+        #logic
+        if product_id in self.reserver:
+            pass
+        else:
+            self.reserver[product_id] = {'price': str(product.price)}
+        
+        self.session.modified = True
+        
+        
+    def __len__(self):
+        return len(self.reserver)
+    
+    def get_prods(self):
+        product_ids = self.reserver.keys()
+        products = Product.objects.filter(id__in=product_ids)
+        return products
--- reserver/views.py
+++ reserver/views.py
@@ -1,13 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .reserver import Reserver
+from serena_app.models import Product
+from django.http import JsonResponse
 
 # Create your views here.
 
 
 def reserver_summary(request):
-    return render(request, "reserver_summary.html", {})
+    #get the cart
+    reserver = Reserver(request)
+    reserver_products = reserver.get_prods
+    return render(request, "reserver_summary.html", {"reserver_products":reserver_products})
 
 def reserver_add(request):
-    pass
+    
+    reserver = Reserver(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('product_id'))
+        product = get_object_or_404(Product, id=product_id)
+        reserver.add(product=product)
+        #get cart quqntity
+        cart_quantity = reserver.__len__()
+        
+        #response = JsonResponse({'Product Name: ': product.name})
+        response = JsonResponse({'qty: ': cart_quantity})
+        return response
+    
+    
 
 def reserver_delete(request):
     pass
